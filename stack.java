183e2435596c7d47df7a6c3d5064f4fe97bcea41
AnimOO7 <43893016+AnimOO7@users.noreply.github.com>
2019-03-12 01:44:17 +0530

    Add files via upload

--- stack.java
+++ stack.java
@@ -1,37 +1,77 @@
-class stack extends arraylist
+class stack extends LinkedList
 {
   void push(int n)
   {
-    add(size,n);
-  }
-  void pop()
-  {
-    System.out.println("The removed element is "+remove(size-1));
-  }
-  void peek()
-  {
-    System.out.println("The topmost element is "+abc[size-1]);
-  }
-  /*int search(int n)
-  {
-    int counter = 1,temp;
-    for (int i=0;i<size;i++)
+    if(START == null)
     {
-      if(abc[i]==n)
-      {
-        counter = 0;
-        temp = i;
-      }
-    }
-    if(counter == 0)
-    {
-      return temp;
+      AddAtStart(n);
     }
     else
     {
-       return -1;
+      AddAtEnd(n);
     }
-  }*/
+  }
+  void pop()
+  {
+    if(size == 0)
+    {
+      System.out.println("Empty stack");
+    }
+    else
+    {
+    System.out.println("The popped element is "+remove(size-1));
+    }
+  }
+  void peep()
+  {
+    Node temp = new Node();
+    temp = START;
+    if(START == null)
+    {
+      System.out.println("empty");
+    }
+    else
+    {
+      while(temp.next!=null)
+      {
+        temp = temp.next;
+      }
+      System.out.println("The topmost element is "+temp.data);
+    }
+  }
+  void search(int n)
+  {
+    if(START == null)
+    {
+      System.out.println("empty");
+    }
+    else
+    {
+      int counter=0;
+      Node temp = new Node();
+      temp = START;
+      while(temp.next!=null)
+      {
+        if(temp.data == n)
+        {
+          counter = 1;
+          break;
+        }
+        else
+        {
+          temp=temp.next;
+        }
+      }
+      if(counter == 1)
+      {
+        System.out.println("Element found");
+      }
+      else
+      {
+        System.out.println("No such element is present");
+      }
+    }
+  }
   void check_size()
   {
     System.out.println("The current size of stack is "+size);
